7bd9a2346637d163c6d0fc6acf09464a2c1bdb89
xushuai <xushvai@gmail.com>
2014-11-19 19:45:20 +0800

    some change
    Signed-off-by: xushuai <xushvai@gmail.com>

--- apps/blog/urls.py
+++ apps/blog/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
-    url(r'^$', 'apps.blog.views.home', name='blog.home'),
+    url(r'^$', 'apps.blog.views.article', name='blog.article'),
     url(r'^toolkit/', 'apps.blog.views.toolkit', name='blog.toolkit'),
     url(r'^works/', 'apps.blog.views.works', name='blog.works'),
     url(r'^about/', 'apps.blog.views.about', name='blog.about'),
--- apps/blog/views.py
+++ apps/blog/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render_to_response
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def home(req):
-    return render_to_response('blog/home.html')
+def article(req):
+    return render_to_response('blog/article.html')
 
 def toolkit(req):
     return render_to_response('blog/toolkit.html')
