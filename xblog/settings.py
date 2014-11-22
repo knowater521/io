a3f41db6c70e4386ac1811275635cbbe0475870d
xushuai <xushvai@gmail.com>
2014-11-22 11:43:16 +0800

    change backend db from sqlite3 to mysql
    Signed-off-by: xushuai <xushvai@gmail.com>

--- xblog/settings.py
+++ xblog/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'redisboard'
 )
 
 MIDDLEWARE_CLASSES = (
@@ -72,8 +73,12 @@ TEMPLATE_DIRS = (
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'xblog',
+        'USER': 'root',
+        'PASSWORD': '123456',
+        'HOST': '127.0.0.1',
+        'PORT': '3306'
     }
 }
 
