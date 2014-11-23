246797ce6c908dbab08d0619ab2a16b60b0c7d62
xushuai <xushvai@gmail.com>
2014-11-23 21:47:49 +0800

    add some code
    Signed-off-by: xushuai <xushvai@gmail.com>

--- apps/blog/models.py
+++ apps/blog/models.py
@@ -1,13 +1,13 @@
 from django.db import models
-
+import datetime
 
 # Create your models here.
 class Article(models.Model):
-    create_timestamp = models.DateTimeField(auto_now_add=True,default=True)
-    update_timestamp = models.DateTimeField(auto_now_add=True,default=True)
+    create_timestamp = models.DateTimeField(auto_now_add=True,default=datetime.datetime.now())
+    update_timestamp = models.DateTimeField(auto_now_add=True,default=datetime.datetime.now())
     title = models.CharField(max_length=64, default='title')
     content = models.TextField(default='content')
     is_published = models.BooleanField(db_index=True, default=True)
 
     def __unicode__(self):
-        return u"[%s](%s)"%(self.title, self.create_timestamp)
+        return u"[%s](create: %s update: %s)"%(self.title, self.create_timestamp, self.update_timestamp)
