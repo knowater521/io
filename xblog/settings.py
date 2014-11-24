7159b38c0cc63865f6fe1ee77bd169905c06030d
xushuai <xushvai@gmail.com>
2014-11-24 17:42:50 +0800

    some change
    Signed-off-by: xushuai <xushvai@gmail.com>

--- xblog/settings.py
+++ xblog/settings.py
@@ -56,7 +56,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    "static",
+    join(BASE_DIR, 'static'),
 )
 
 ROOT_URLCONF = 'xblog.urls'
