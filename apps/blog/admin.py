07edffeb44cef56331d61f199aff23990b28a0d6
xushuai_rmbp <xushvai@gmail.com>
2015-05-03 22:22:53 +0800

    del old files and dirs.
    Signed-off-by: xushuai_rmbp <xushvai@gmail.com>

--- apps/blog/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Article
-
-class ArticleAdmin(admin.ModelAdmin):
-    date_hierarchy = 'create_timestamp'
-
-admin.site.register(Article, ArticleAdmin)
--- apps/blog/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import patterns, include, url
-
-urlpatterns = patterns('',
-    url(r'^$', 'apps.blog.views.article', name='blog.article'),
-    url(r'^toolkit/', 'apps.blog.views.toolkit', name='blog.toolkit'),
-    url(r'^works/', 'apps.blog.views.works', name='blog.works'),
-    url(r'^about/', 'apps.blog.views.about', name='blog.about'),
-)
