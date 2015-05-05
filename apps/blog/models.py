58999031d53c50e78443b3142058e1e0776b6a2b
xushuai_rmbp <xushvai@gmail.com>
2015-05-05 09:50:47 +0800

    add Category model.
    Signed-off-by: xushuai_rmbp <xushvai@gmail.com>

--- apps/blog/models.py
+++ apps/blog/models.py
@@ -2,14 +2,21 @@ from django.db import models
 import datetime
 
 class Article(models.Model):
-    title = models.CharField(max_length=200)
+    title = models.CharField(max_length=255)
     #tags = models.ManyToManyField()
-    author = models.CharField(max_length=100, default='anonymous')
+    author = models.CharField(max_length=128, default='anonymous')
     create_date = models.DateField(auto_now_add=True, default=datetime.datetime.now())
     modify_date = models.DateField(auto_now=True, default=datetime.datetime.now())
     content = models.TextField(default='')
     is_publish = models.BooleanField(default=True)
 
-
     def __str__(self):
         return  self.content
+
+class Category(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    desc = models.CharField(max_length=255, unique=True)
+    slug = models.SlugField(max_length=255)
+
+    def __str__(self):
+        return 'name: ' + self.name + 'desc: ' + self.desc + 'slug: ' + self.slug
