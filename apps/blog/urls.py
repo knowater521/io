07bfa7c1d2033787dbaa700d137deb901ce3dda0
xushuai_rmbp <xushvai@gmail.com>
2015-05-05 21:05:59 +0800

    some change.
    Signed-off-by: xushuai_rmbp <xushvai@gmail.com>

--- apps/blog/urls.py
+++ apps/blog/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
 from . import views
 
-urlpatterns = [
+urlpatterns = (
     url(r'^$', views.Index, name='index'),
-]
+    url(r'^works/', views.Works, name='works'),
+    url(r'^about/', views.About, name='about'),
+)
--- apps/blog/views.py
+++ apps/blog/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
+from django.shortcuts import render_to_response
 from django.http import HttpResponse
 from .models import Article
 
 def Index(req):
+    articles = Article.objects.all()
+    context = {'articles': articles}
+    return render_to_response('index.html', context)
+
+def Works(req):
     context = {'title': 'sfsdxxxxf'}
-    return render(req, 'index.html', context)
+    return render_to_response('works.html', context)
+
+def About(req):
+    context = {'title': 'sfsdxxxxf'}
+    return render_to_response('about.html', context)
