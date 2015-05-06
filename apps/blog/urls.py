0caeff7a9dba5d72b68f02fcdd387b283c9c8ddc
xushuai_rmbp <xushvai@gmail.com>
2015-05-06 11:29:11 +0800

    some change.
    Signed-off-by: xushuai_rmbp <xushvai@gmail.com>

--- apps/blog/urls.py
+++ apps/blog/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = (
-    url(r'^article/(?P<slug>\S+)/$', views.ArticleDetailView.as_view(), name='detail'),
+    url(r'^article/(?P<slug>\S+)/$', views.ArticleDetailView.as_view(), name='article_detail'),
     url(r'^$', views.Index, name='index'),
     url(r'^works/', views.Works, name='works'),
     url(r'^about/', views.About, name='about'),
