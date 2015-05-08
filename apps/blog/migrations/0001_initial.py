3ba978db9eb938eda22bcab3001717a74982f83e
xushuai_rmbp <xushvai@gmail.com>
2015-05-08 11:39:36 +0800

    use django 1.8 version.
    Signed-off-by: xushuai_rmbp <xushvai@gmail.com>

--- apps/blog/migrations/0001_initial.py
+++ apps/blog/migrations/0001_initial.py
@@ -2,7 +2,7 @@
 from __future__ import unicode_literals
 
 from django.db import models, migrations
-import datetime
+import django_markdown.models
 
 
 class Migration(migrations.Migration):
@@ -15,15 +15,48 @@ class Migration(migrations.Migration):
             name='Article',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('title', models.CharField(max_length=200)),
-                ('author', models.CharField(default=b'anonymous', max_length=100)),
-                ('create_date', models.DateField(default=datetime.datetime(2015, 5, 4, 10, 20, 47, 591595), auto_now_add=True)),
-                ('modify_date', models.DateField(default=datetime.datetime(2015, 5, 4, 10, 20, 47, 591619), auto_now=True)),
-                ('content', models.TextField(default=b'')),
+                ('title', models.CharField(max_length=255)),
+                ('author', models.CharField(default=b'anonymous', max_length=128)),
+                ('create_date', models.DateField(auto_now_add=True)),
+                ('modify_date', models.DateField(auto_now=True)),
+                ('text', django_markdown.models.MarkdownField(null=True)),
                 ('is_publish', models.BooleanField(default=True)),
+                ('slug', models.SlugField(unique=True, max_length=255)),
             ],
             options={
+                'verbose_name_plural': '\u6587\u7ae0',
             },
-            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Category',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(unique=True, max_length=255)),
+                ('desc', models.CharField(unique=True, max_length=255)),
+                ('slug', models.SlugField(max_length=255)),
+            ],
+            options={
+                'verbose_name_plural': '\u7c7b\u578b',
+            },
+        ),
+        migrations.CreateModel(
+            name='Tag',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('slug', models.SlugField(unique=True, max_length=128)),
+            ],
+            options={
+                'verbose_name_plural': '\u6807\u7b7e',
+            },
+        ),
+        migrations.AddField(
+            model_name='article',
+            name='category',
+            field=models.ForeignKey(to='blog.Category'),
+        ),
+        migrations.AddField(
+            model_name='article',
+            name='tag',
+            field=models.ManyToManyField(to='blog.Tag'),
         ),
     ]
