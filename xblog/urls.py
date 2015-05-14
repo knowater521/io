5ce26c82e2e5719a9b78466e0c8ed3cfa6b3d59b
xus <xushvai@gmail.com>
2015-05-14 19:14:53 +0800

    modify urls settings.
    Signed-off-by: xus <xushvai@gmail.com>

--- xblog/urls.py
+++ xblog/urls.py
@@ -3,13 +3,11 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 
+urlpatterns = [
+	url(r'^adminfiles/', include('adminfiles.urls')),
+    	url(r'^admin/', include(admin.site.urls)),
+    	url(r'^markdown/', include('django_markdown.urls')),
+    	#url(r'^', include('apps.blog.urls')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'xblog.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-    url(r'^adminfiles/', include('adminfiles.urls')),
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^markdown/', include('django_markdown.urls')),
-    #url(r'^', include('apps.blog.urls')),
-) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += url(r'^', include('apps.blog.urls')),
