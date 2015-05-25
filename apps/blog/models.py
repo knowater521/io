fdebf178ce5514acadd1f7eb86846a29bf00754b
xus <xushvai@gmail.com>
2015-05-26 01:47:49 +0800

    implement tag cloud.
    Signed-off-by: xus <xushvai@gmail.com>

--- apps/blog/models.py
+++ apps/blog/models.py
@@ -33,18 +33,20 @@ class Tag(models.Model):
 
     def __unicode__(self):
         return self.name
+    
+    def GetArticleNum(self):
+        return Article.objects.filter(tag=self).count()
    
     def CalFontSizeOfTag(self):
         min_font_size = 11
         max_font_size = 35
+        tag_ref_num = self.GetArticleNum()
         # blog_article_tag table
         # select max(tag_ref_num) from(select count(*) as tag_ref_num from blog_article_tag group by tag_id) as max_ref_num;
         max_tag_num = Article.tag.through.objects.values('tag_id').annotate(tag_ref_num=Count('*')).aggregate(max_ref_num=Max('tag_ref_num'))['max_ref_num']
-    
-    def GetArticleNum(self):
-        self.CalFontSizeOfTag()
-        return Article.objects.filter(tag=self).count()
-        
+        if max_tag_num <= 0:
+            tag_ref_num = 0
+        return min_font_size + tag_ref_num * (max_font_size - min_font_size) / max_tag_num
 
     def GetAbsoluteURL(self):
         return reverse('tag_home', kwargs={'slug':self.slug})
