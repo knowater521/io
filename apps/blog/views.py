68a32d38368491903d0039c35593ceb3c5d4ac5a
xushuai_rmbp <xushvai@gmail.com>
2015-07-15 15:58:36 +0800

    just change name.
    Signed-off-by: xushuai_rmbp <xushvai@gmail.com>

--- apps/blog/views.py
+++ apps/blog/views.py
@@ -9,7 +9,7 @@ from .models import Article, Category, Tag, Config
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render, render_to_response, get_object_or_404
 
-def PagingForArticles(articles, per_page, page_idx):
+def PaginateArticles(articles, per_page, page_idx):
     paginator = Paginator(articles, per_page)
     try:
         articles = paginator.page(page_idx)
@@ -25,7 +25,7 @@ def Home(req):
     if suffix == 'html' or suffix == 'htm':
         return render_to_response(static_page)
     articles = Article.objects.order_by('-create_date')
-    articles = PagingForArticles(articles, 6, req.GET.get('page'))
+    articles = PaginateArticles(articles, 6, req.GET.get('page'))
     context = {'articles':articles, 'nbar':'index'}
     return render_to_response('home.html', context)
 
@@ -72,14 +72,14 @@ def Archives(req):
 def ArticlesOfTag(req, slug):
     cur_tag = get_object_or_404(Tag, slug=slug)
     articles = Article.objects.filter(tag=cur_tag).order_by('-create_date')
-    articles = PagingForArticles(articles, 6, req.GET.get('page'))
+    articles = PaginateArticles(articles, 6, req.GET.get('page'))
     context = {'articles':articles, 'nbar':'tags_home'}
     return render_to_response('articles_of_tag.html', context)
 
 def ArticlesOfCategory(req, slug):
     cur_category = get_object_or_404(Category, slug=slug)
     articles = Article.objects.filter(category=cur_category).order_by('-create_date')
-    articles = PagingForArticles(articles, 6, req.GET.get('page'))
+    articles = PaginateArticles(articles, 6, req.GET.get('page'))
     context = {'articles':articles, 'nbar':'categories_home'}
     return render_to_response('articles_of_category.html', context)
 
