32f32f839cd96f57beb7bba7166c418e99f210c0
xushuai_rmbp <xushvai@gmail.com>
2015-07-17 00:42:38 +0800

    some change.
    Signed-off-by: xushuai_rmbp <xushvai@gmail.com>

--- apps/blog/search_indexes.py
+++ apps/blog/search_indexes.py
@@ -4,12 +4,12 @@ from .models import Article
 
 class ArticleIndex(indexes.SearchIndex, indexes.Indexable):
 	text = indexes.CharField(document=True, use_template=True)
-    author = indexes.CharField(model_attr='author')
-    create_date = indexes.DateTimeField(model_attr='create_date')
+	author = indexes.CharField(model_attr='author')
+	create_date = indexes.DateTimeField(model_attr='create_date')
 
-    def get_model(self):
+	def get_model(self):
 		return Article
 
-    def index_queryset(self, using=None):
-        """Used when the entire index for model is updated."""
-        return self.get_model().objects.filter(create_date__lte=datetime.datetime.now())
+	def index_queryset(self, using=None):
+		"""Used when the entire index for model is updated."""
+		return self.get_model().objects.filter(create_date__lte=datetime.datetime.now())
