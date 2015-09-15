e497b692a8c700bbc406c7418c8e1d32a44d3cd4
xus <xushvai@gmail.com>
2015-09-15 23:26:42 +0800

    some change.
    Signed-off-by: xus <xushvai@gmail.com>

--- xblog/settings_pro.py
+++ xblog/settings_pro.py
@@ -7,9 +7,17 @@ PRODUCTION_SECRET_FILE = '/data/production_secret.json'
 DEBUG = TEMPLATE_DEBUG = False
 ALLOWED_HOSTS = ['.xus.io']
 
-with open(PRODUCTION_SECRET_FILE) as json_file:
-	data = json.load(json_file)
-SECRET_KEY = data['SECRET_KEY']
+if True == os.path.exists(PRODUCTION_SECRET_FILE):
+	with open(PRODUCTION_SECRET_FILE) as json_file:
+		data = json.load(json_file)
+		SECRET_KEY = data['SECRET_KEY']
+		DATABASES['default']['USER'] = data['DB']['USER']
+		DATABASES['default']['PASSWORD'] = data['DB']['PASSWORD']
+
+DATABASES['default']['ENGINE'] = 'django.db.backends.mysql'
+DATABASES['default']['NAME'] = 'xblog'
+DATABASES['default']['HOST'] = '127.0.0.1'
+DATABASES['default']['PORT'] = '3306'
 
 SESSION_COOKIE_SECURE = False
 CSRF_COOKIE_SECURE = False
@@ -18,13 +26,6 @@ X_FRAME_OPTIONS = 'DENY'
 
 INSTALLED_APPS += ()
 MIDDLEWARE_CLASSES += ()
-
-DATABASES['default']['USER'] = data['DB']['USER']
-DATABASES['default']['PASSWORD'] = data['DB']['PASSWORD']
-DATABASES['default']['ENGINE'] = 'django.db.backends.mysql'
-DATABASES['default']['NAME'] = 'xblog'
-DATABASES['default']['HOST'] = '127.0.0.1'
-DATABASES['default']['PORT'] = '3306'
 
 STATIC_ROOT = '/data/static/'
 MEDIA_ROOT = '/data/media/' 
