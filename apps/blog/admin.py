3d84cfc4b462d74423af0de0f15b810fecafb355
xushuai_rmbp <xushvai@gmail.com>
2015-09-17 20:29:52 +0800

    disable delete for User model.
    Signed-off-by: xushuai_rmbp <xushvai@gmail.com>

--- apps/blog/admin.py
+++ apps/blog/admin.py
@@ -29,6 +29,8 @@ class MyAdmin(admin.ModelAdmin):
     )
 	def has_add_permission(self, request):
 		return False
+	def has_delete_permission(self, request, obj=None):
+		return False
 
 admin.site.unregister(User)
 admin.site.unregister(Group)
