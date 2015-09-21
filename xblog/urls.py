0debbc31e897eb063c450194a8dc179334f44da6
xushuai_rmbp <xushvai@gmail.com>
2015-09-21 19:09:47 +0800

    some change.
    Signed-off-by: xushuai_rmbp <xushvai@gmail.com>

--- xblog/urls.py
+++ xblog/urls.py
@@ -2,7 +2,6 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
-
 from haystack.views import SearchView
 from haystack.query import SearchQuerySet
 
