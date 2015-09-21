3e2be7116f74075723372a0f96c2730fbbd6e44f
xushuai_rmbp <xushvai@gmail.com>
2015-09-21 14:18:54 +0800

    make search result order by create_date.
    Signed-off-by: xushuai_rmbp <xushvai@gmail.com>

--- xblog/urls.py
+++ xblog/urls.py
@@ -3,12 +3,17 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 
+from haystack.views import SearchView
+from haystack.query import SearchQuerySet
+
+sqs = SearchQuerySet().order_by('-create_date')
+
 urlpatterns = [
     url(r'^grappelli/', include('grappelli.urls')),
 	url(r'^adminfiles/', include('adminfiles.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^markdown/', include('django_markdown.urls')),
-    url(r'^search/', include('haystack.urls')),
+    url(r'^search/', SearchView(load_all=False, searchqueryset=sqs,),name='haystack_search',),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 urlpatterns += url(r'^', include('apps.blog.urls')),
